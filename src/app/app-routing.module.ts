8714d22034825e0d5fe79e17acb67ed832155193
Sridhar <37789329+sridharsri16@users.noreply.github.com>
2020-08-07 23:30:31 +0530

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HackerrecipedetailsComponent } from './hackerrecipe/hackerrecipedetails.component';
+import { HackerrecipeComponent } from './hackerrecipe/hackerrecipe.component';
 
 
 const routes: Routes = [
   {
     path: "hackerrecepiedetails/:id", component: HackerrecipedetailsComponent,
   },
+  
+  { path: "", component: HackerrecipeComponent, },
+  { path: '**', component: HackerrecipeComponent, }
 ];
 
 @NgModule({
